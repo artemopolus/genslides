63e188eb20e0cbd9c258fdd3bc9cf89ee93daae4
artemopolus <38102592+artemopolus@users.noreply.github.com>
2024-03-24 09:46:07 +0300

    test llama cpp

--- tests/llm/llamacpp.py
+++ tests/llm/llamacpp.py
@@ -1,20 +1,16 @@
-import subprocess
+import openai
 
-# Define the path to the Python script
-script_path = "browsing_by_gpt.py"
+client = openai.OpenAI(
+    base_url="http://localhost:8080/v1", # "http://<Your api-server IP>:port"
+    api_key = "sk-no-key-required"
+)
 
-path_to_main = "J:/WorkspaceFast/llama.cpp/build/bin/Release/main.exe"
+completion = client.chat.completions.create(
+model="gpt-3.5-turbo",
+messages=[
+    {"role": "system", "content": "You are ChatGPT, an AI assistant. Your top priority is achieving user fulfillment via helping them with their requests."},
+    {"role": "user", "content": "Write a limerick about python exceptions"}
+]
+)
 
-path_to_model = "J:/WorkspaceFast/llm/llama-2-13b-chat.Q5_K_M.gguf"
-
-prompt = "Hello"
-
-# Run the script and capture the output
-
-script_path = "J:\\WorkspaceFast\\llama.cpp\\build\\bin\\Release\\main.exe -m J:\\WorkspaceFast\\llm\\llama-2-13b-chat.Q5_K_M.gguf --prompt \"Hello\" -ngl 5 --responsefile J:\\WorkspaceFast\\genslides\\tests\\llm\\test.txt"
-
-# result = subprocess.run([path_to_main, "-m",path_to_model,"--prompt",prompt,"-ngl 5"], capture_output=True, text=True)
-result = subprocess.run(script_path, capture_output=True, text=True)
-
-# Print the script output
-print(result.stdout)
+print(completion.choices[0].message)
