0e960df0fd3b6b38044f046161843064d142ba3f
artemopolus <38102592+artemopolus@users.noreply.github.com>
2024-08-05 11:35:02 +0300

    config files copy

--- genslides/test/test_create_project.py
+++ genslides/test/test_create_project.py
@@ -1,6 +1,7 @@
 # genslides/test/test_create_project.py
 
 import os
+import shutil
 import unittest
 from create_project import create_project, count_created_files
 
@@ -9,11 +10,23 @@ class TestCreateProject(unittest.TestCase):
     def setUp(self):
         # Set up the path where files are saved
         self.project_path = 'saved/'
+        self.config_path = 'config/'
+        self.examples_config_path = 'examples/config/'
+
+        # Create 'saved/' directory and clean it if it exists
         if os.path.exists(self.project_path):
             for f in os.listdir(self.project_path):
                 os.remove(os.path.join(self.project_path, f))
         else:
             os.makedirs(self.project_path)
+
+        # Check if 'config/' directory exists, if not, create it
+        if not os.path.exists(self.config_path):
+            os.makedirs(self.config_path)
+            # Copy files from 'examples/config' to 'config'
+            for filename in os.listdir(self.examples_config_path):
+                source_file = os.path.join(self.examples_config_path, filename)
+                shutil.copy(source_file, self.config_path)
 
     def test_create_files(self):
         # Run the project creation
